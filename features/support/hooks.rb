17acc6d9d42bb63c35249f31447c33380d953e61
Reiner Miller-Basile <reiner23fx@gmail.com>
2013-10-21 10:32:23 +0200

    magento api - order cancelation

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,2 +1,8 @@
 Capybara.app_host = "http://google.com"
 Capybara.default_driver = :selenium
+Capybara.default_wait_time = 5
+
+
+# at_exit do
+# 	MagentoAPI::cancel_test_orders("exampleuser@example.com","API_USERNAME","API_PASSWORD")
+# end
